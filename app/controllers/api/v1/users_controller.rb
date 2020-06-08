644658e51b2529baaed77a08d8c39215555c20e9
“Krista <stadler.krista@gmail..com>
2020-06-07 21:30:06 -0600

    Finished testing sad path for user registration

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,12 +4,22 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     if confirm_password?
-      user = User.create!(email: params[:email],
+      user = User.new(email: params[:email],
                    password: params[:password],
                    api_key: SecureRandom.urlsafe_base64 )
-                   
+      if user.save
+        render json: UserSerializer.new(user), status: :created
+      else
+        create_error = {
+                "error_message" => user.errors.full_messages.to_sentence
+               }
+        render json: create_error, status: 400
+      end
     else
-      p "Sorry, could not finish your request"
+      password_error = {
+              "error_message" => "Password and password confirmation must match"
+             }
+      render json: password_error, status: 400
     end
   end
 
--- spec/requests/api/v1/registration_requests_spec.rb
+++ spec/requests/api/v1/registration_requests_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 describe 'Registration API' do
   it "registers a user in the database and returns their login information" do
-    # json = {"data" =>
-    #   { "email": "whatever@example.com",
-    #     "password": "password",
-    #     "password_confirmation": "password"}}
 
     post '/api/v1/users', params: { email: "whatever@example.com",
       password: "password",
@@ -16,6 +12,33 @@ describe 'Registration API' do
 
     body = JSON.parse(response.body)
 
-    require "pry"; binding.pry
+  end
+
+  it "does not register a user if the password and password confirmation do not match" do
+
+    post '/api/v1/users', params: { email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "passwrod" }
+
+    body = JSON.parse(response.body)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(body["error_message"]).to eq("Password and password confirmation must match")
+
+  end
+
+  it "does not register a user if the password and password confirmation do not match" do
+    user = User.create(email: "whatever@example.com",
+                      api_key: "q1tQHkkMyKpfYPqWD7-2cQ4",
+                      password: "password")
+
+    post '/api/v1/users', params: { email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password" }
+
+    body = JSON.parse(response.body)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    expect(body["error_message"]).to eq("Email has already been taken")
   end
 end
