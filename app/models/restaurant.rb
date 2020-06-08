279601f21f2d15f29a24750ec77ab31283841e40
“Krista <stadler.krista@gmail..com>
2020-06-08 11:37:19 -0600

    Cleaned up files

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,8 +5,6 @@ class Restaurant
   def initialize(attributes)
     @name = attributes[:restaurants].first[:restaurant][:name]
     @address = [attributes[:restaurants].first[:restaurant][:location][:address], attributes[:restaurants].first[:restaurant][:location][:city], attributes[:restaurants].first[:restaurant][:location][:zipcode]].join(' ')
-    # @city = attributes[:restaurants].first[:restaurant][:location][:city]
-    # @zip = attributes[:restaurants].first[:restaurant][:location][:zipcode]
   end
 
 end
