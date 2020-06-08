9ff4a171daacf8b7b6dfc6ea7d485f89f8e8c37e
“Krista <stadler.krista@gmail..com>
2020-06-08 09:15:19 -0600

    Wrote test

---
+++ spec/requests/api/v1/foodie_requests_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Foodie API' do
+  it "retrieves food and forecast information for a destination city" do
+
+    get "/api/v1/foodie?start=denver,co&end=pueblo,co&search=italian"
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(results[:data][:id]).to eq("null")
+    expect(results[:data][:type]).to eq("foodie")
+    expect(results[:data][:attributes][:end_location]).to eq("pueblo, co")
+    expect(results[:data][:restaurant][:address].downcase).to include("pueblo")
+    
+  end
+end
