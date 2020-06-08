cb048df9e35fffeb22b9b154ce140ff081cbdf31
“Krista <stadler.krista@gmail..com>
2020-06-08 14:20:25 -0600

    Wrote test for login

--- spec/requests/api/v1/registration_requests_spec.rb
+++ spec/requests/api/v1/registration_requests_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Registration API' do
+describe 'Users API' do
   it "registers a user in the database and returns their login information" do
 
     post '/api/v1/users', params: { email: "whatever@example.com",
@@ -41,4 +41,24 @@ describe 'Registration API' do
     expect(response.status).to eq(400)
     expect(body["error_message"]).to eq("Email has already been taken")
   end
+
+  it "returns a users information when a session is started" do
+    user = User.create(email: "whatever@example.com",
+                      api_key: "q1tQHkkMyKpfYPqWD7-2cQ4",
+                      password: "password")
+
+    post '/api/v1/sessions', params { email: "whatever@example.com",
+                                      password: "password" }
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(body[:data][:type]).to eq("users")
+    expect(body[:data][:id]).to eq(user.id)
+    expect(body[:data][:attributes][:email]).to eq(user.email)
+    expect(body[:data][:attributes][:api_key]).to eq(user.api_key)
+
+  end
+
 end
