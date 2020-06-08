522001faddf4901330f9b1b8f2a2464b9b82f0b2
“Krista <stadler.krista@gmail..com>
2020-06-07 21:36:42 -0600

    Added validations tests to user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true, presence: true
   has_secure_password
-  validates :password_digest, presence: true 
+  validates :password_digest, presence: true
 end
