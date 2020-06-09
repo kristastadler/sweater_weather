21ba8b064d91431c2e3e4b24234262ef0c44ba1e
“Krista <stadler.krista@gmail..com>
2020-06-09 09:16:03 -0600

    Wrote test for road trip

---
+++ spec/requests/api/v1/roadtrip_requests_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Roadtrip API' do
+  it "retrieves travel time and arrival forecast for a road trip" do
+    user = User.create(email: "whatever@example.com",
+                      api_key: "q1tQHkkMyKpfYPqWD7-2cQ4",
+                      password: "better_password")
+
+    post '/api/v1/road_trip', params: { origin: "Denver, CO",
+      destination: "Pueblo, CO",
+      api_key: user.api_key }
+
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(results[:data][:type]).to eq("road_trip")
+    expect(results[:data][:attributes][:origin]).to eq("Denver, CO")
+    expect(results[:data][:attributes][:destination]).to eq("Pueblo, CO")
+    expect(results[:data][:attributes][:travel_time]).to eq("1 hour 48 mins")
+    expect(results[:data][:attributes][:forecast][:description]).to_not eq("nil")
+    expect(results[:data][:attributes][:forecast][:temperature]).to_not eq("nil")
+  end
+end
